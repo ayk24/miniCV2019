ca6c2235c8cf3ce02bb9ae0e5f35e797af9df8d7
Ayaka <ayk.04ls.o0@gmail.com>
2019-09-19 01:04:34 +0900

    課題1 まで
    コード生成テスト済
    (MiniCompiler.java)

--- src/lang/c/parse/Expression.java
+++ src/lang/c/parse/Expression.java
@@ -162,10 +162,10 @@ class ExpressionSub extends CParseRule {
 		if (left != null && right != null) {
 			left.codeGen(pcx);		// 左部分木のコード生成を頼む
 			right.codeGen(pcx);		// 右部分木のコード生成を頼む
-			o.println("\tMOV\t-(R6), R0\t; ExpressionSub: ２数を取り出して、引き、積む<" + op.toString() + ">");
-			o.println("\tMOV\t-(R6), R1\t; ExpressionSub:");
-			o.println("\tSUB\tR0, R1\t; ExpressionSub:");
-			o.println("\tMOV\tR1, (R6)+\t; ExpressionSub:");
+			o.println("\tMOV\t-(R6), R1\t; ExpressionSub: ２数を取り出して、引き、積む<" + op.toString() + ">");
+			o.println("\tMOV\t-(R6), R0\t; ExpressionSub:");
+			o.println("\tSUB\tR1, R0\t\t; ExpressionSub:");
+			o.println("\tMOV\tR0, (R6)+\t; ExpressionSub:");
 		}
 	}
 }
--- src/lang/c/parse/Program.java
+++ src/lang/c/parse/Program.java
@@ -1,8 +1,12 @@
 package lang.c.parse;
 
 import java.io.PrintStream;
-import lang.*;
-import lang.c.*;
+
+import lang.FatalErrorException;
+import lang.c.CParseContext;
+import lang.c.CParseRule;
+import lang.c.CToken;
+import lang.c.CTokenizer;
 
 public class Program extends CParseRule {
 	// program ::= expression EOF
@@ -32,7 +36,7 @@ public class Program extends CParseRule {
 		PrintStream o = pcx.getIOContext().getOutStream();
 		o.println(";;; program starts");
 		o.println("\t. = 0x100");
-		o.println("\tJMP\t__START\t; ProgramNode: 最初の実行文へ");
+		o.println("\tJMP\t__START\t\t; ProgramNode: 最初の実行文へ");
 		// ここには将来、宣言に対するコード生成が必要
 		if (program != null) {
 			o.println("__START:");
@@ -40,7 +44,7 @@ public class Program extends CParseRule {
 			program.codeGen(pcx);
 			o.println("\tMOV\t-(R6), R0\t; ProgramNode: 計算結果確認用");
 		}
-		o.println("\tHLT\t\t\t; ProgramNode:");
+		o.println("\tHLT\t\t\t\t; ProgramNode:");
 		o.println("\t.END\t\t\t; ProgramNode:");
 		o.println(";;; program completes");
 	}
