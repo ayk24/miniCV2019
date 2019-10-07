bf1df03668833c2fb83c207040d342f59bba5b5f
Ayaka <ayk.04ls.o0@gmail.com>
2019-10-07 13:38:46 +0900

    課題 1.8まで
    Expression ::
    codeGenの部分のtabが足りなかったので, 付け足した.

--- src/lang/c/parse/Expression.java
+++ src/lang/c/parse/Expression.java
@@ -106,7 +106,7 @@ class ExpressionAdd extends CParseRule {
 			right.codeGen(pcx);		// 右部分木のコード生成を頼む
 			o.println("\tMOV\t-(R6), R0\t; ExpressionAdd: ２数を取り出して、足し、積む<" + op.toString() + ">");
 			o.println("\tMOV\t-(R6), R1\t; ExpressionAdd:");
-			o.println("\tADD\tR1, R0\t; ExpressionAdd:");
+			o.println("\tADD\tR1, R0\t\t; ExpressionAdd:");
 			o.println("\tMOV\tR0, (R6)+\t; ExpressionAdd:");
 		}
 	}
